bec2f241decee5747c176f958de869a6a94b0f14
shmygindmitrii <shmygindmitrii@gmail.com>
2024-05-25 09:40:29 +0800

    make default crc32_ccit value const

--- Lowfat/Lowfat/crc32_ccit.h
+++ Lowfat/Lowfat/crc32_ccit.h
@@ -6,7 +6,7 @@
 extern "C" {
 #endif
 
-extern uint32_t CRC32_CCIT_DEFAULT_VALUE;
+extern const uint32_t CRC32_CCIT_DEFAULT_VALUE;
 uint32_t crc32_ccit_update(uint8_t * data, uint32_t size, uint32_t crc);
 
 #ifdef __cplusplus
