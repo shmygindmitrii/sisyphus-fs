c1698eb44430f8e1201352e87b329ead4150732e
shmygindmitrii <shmygindmitrii@gmail.com>
2024-07-02 23:32:17 +0800

    cleanup .lua scripts for premake
    add Level4 warnings and Warnings as Errors flag

--- premake/projects/linkfs.lua
+++ premake/projects/linkfs.lua
@@ -1,14 +1,30 @@
 project("linkfs")
     kind("StaticLib")
     targetname("linkfs")
-    files({"../../source/linkfs/src/linkfs.c", 
- 	   "../../source/linkfs/inc/linkfs.h",
- 	   "../../source/linkfs/inc/linkfs_defines.h",	
- 	   "../../source/linkfs/inc/linkfs_prelude.h"})
-    includedirs({"../../source/linkfs/inc/", "../../source/crc/inc/"})
+    files({
+        "../../source/linkfs/src/linkfs.c", 
+ 	"../../source/linkfs/inc/linkfs.h",
+ 	"../../source/linkfs/inc/linkfs_defines.h",	
+ 	"../../source/linkfs/inc/linkfs_prelude.h"
+    })
+    includedirs({
+        "../../source/linkfs/inc/", 
+        "../../source/crc/inc/"
+    })
     location("../../build/linkfs")
     targetdir("../../build/linkfs/bin")
     objdir("../../build/linkfs/obj")
     exceptionhandling("On")
-    defines { "_LIB", "LINKFS_CUSTOM_DEBUGBREAK", "LINKFS_CUSTOM_ALLOCATOR", "LINKFS_ASSERT_ENABLED" }
-    links {"crc"}
+    defines { 
+        "_LIB", 
+        "LINKFS_CUSTOM_DEBUGBREAK", 
+        "LINKFS_CUSTOM_ALLOCATOR", 
+        "LINKFS_ASSERT_ENABLED" 
+    }
+    links {
+        "crc"
+    }
+    warnings "Extra"
+    flags { 
+        "FatalCompileWarnings" 
+    }
--- premake/projects/lowfatfs.lua
+++ premake/projects/lowfatfs.lua
@@ -1,17 +1,33 @@
 project("lowfatfs")
     kind("StaticLib")
     targetname("lowfatfs")
-    files({"../../source/lowfatfs/src/lowfatfs.c", 
- 	   "../../source/lowfatfs/inc/lowfatfs.h",
- 	   "../../source/lowfatfs/inc/lowfatfs_defines.h",	
- 	   "../../source/lowfatfs/inc/lowfatfs_prelude.h",
-	   "../../source/structures/inc/structures.h",
-	   "../../source/structures/inc/structures_prelude.h"})
-    includedirs({"../../source/lowfatfs/inc/", "../../source/structures/inc/", "../../source/crc/inc/"})
+    files({
+	"../../source/lowfatfs/src/lowfatfs.c", 
+ 	"../../source/lowfatfs/inc/lowfatfs.h",
+ 	"../../source/lowfatfs/inc/lowfatfs_defines.h",	
+ 	"../../source/lowfatfs/inc/lowfatfs_prelude.h",
+	"../../source/structures/inc/structures.h",
+        "../../source/structures/inc/structures_prelude.h"
+    })
+    includedirs({
+        "../../source/lowfatfs/inc/", 
+        "../../source/structures/inc/", 
+        "../../source/crc/inc/"
+    })
     location("../../build/lowfatfs")
     targetdir("../../build/lowfatfs/bin")
     objdir("../../build/lowfatfs/obj")
     exceptionhandling("On")
-    defines { "_LIB", "LOWFATFS_VERBOSITY=LOWFATFS_VERBOSITY_DETAILED", "LOWFATFS_CUSTOM_ALLOCATOR", "LOWFATFS_ASSERT_ENABLED" }
-    links {"crc"}
-    --libdirs {"../../build/crc/lib"}
+    defines { 
+        "_LIB", 
+        "LOWFATFS_VERBOSITY=LOWFATFS_VERBOSITY_DETAILED", 
+        "LOWFATFS_CUSTOM_ALLOCATOR", 
+        "LOWFATFS_ASSERT_ENABLED" 
+    }
+    links {
+        "crc"
+    }
+    warnings "Extra"
+    flags { 
+        "FatalCompileWarnings" 
+    }
